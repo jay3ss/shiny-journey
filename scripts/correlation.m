923eba12d17b942d2d590baa8db6c9514bd436d0
Jay Ess <mentoce@gmail.com>
2018-03-13 14:40:58 -0700

    Operate on just one matrix/feature at a time, no plotting

--- scripts/correlation.m
+++ scripts/correlation.m
@@ -1,4 +1,4 @@
-function [corr_mat] = perform_pca(F0,data_train_norm,data_test_norm)
+function [auto_corr, feature_corr] = correlation(data, feature_col)
 % this function performs PCR based regression of the feature F0 using the 
 % features of data_train_norm and uses the regression weights to estimate
 % the feature F0 of data_test_norm. Evaluates accuracy and visualized
@@ -8,28 +8,13 @@ function [corr_mat] = perform_pca(F0,data_train_norm,data_test_norm)
 % nbins: number of bins used in histogram
 % data_train_norm: prepared and normalized data for training
 % data_test_norm: prepared and normalized data for testing
-[N,F]=size(data_train_norm);
-RX_train=data_train_norm'*data_train_norm/N;
-[U,Lambda] = eig(RX_train);
-Z_train=data_train_norm*U;
-Zx_train=Z_train;
-Zx_train(:,F0)=[];
-y_train=Z_train(:,F0);               % measured data on feature F0
-Z_test=data_test_norm*U;
-Zx_test=Z_test;
-Zx_test(:,F0)=[];
-y_test=Z_test(:,F0);             % measured data on feature F0y_test=data_test_norm(:,F0);         % measured data on feature F0
+[rows, cols] = size(data);
+RX = data'*data/rows; 
+[U, lambda] = eig(RX);
+z = data*U;
+z_feature = z(:, feature_col);
+z(:, feature_col) = [];
 
 % correlation between feature 7 and the other features in the new basis
-corr7=corr(Z_train(:,7),Z_train);
-figure
-plot(corr7)
-grid on
-
-% autocorrelation matrix
-corr_mat=corr(Z_train)
-
-figure
-mesh(corr_mat)
-grid on
-title('correlation matrix');
+feature_corr = corr(Z_train(:,7),Z_train);
+auto_corr = corr(Z_train); % autocorrelation matrix
